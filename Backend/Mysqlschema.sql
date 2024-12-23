b6469a3ddab69aacdb78be1e8eb415b387c22562
Enzo Terenziani <145686428+im23b-terenzianie@users.noreply.github.com>
2024-12-23 15:34:52 +0100

    Delete Backend directory

--- Backend/Mysqlschema.sql
+++
@@ -1,7 +0,0 @@
-create database kalenderproject;
-use kalenderproject;
-create table user(
-	userid int primary key auto_increment,
-    email varchar(255) unique not null,
-	pw varchar(255) not null
-);
