d3e6f50e36af1efe94a74d041607dffe27ca93d2
im23b-terenzianie <terenzianie@bzz.ch>
2024-12-27 15:11:20 +0100

    Register Button added to Login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -14,9 +14,12 @@ export default function Login() {
                     <label className="text-white dark:text-black">Password</label>
                     <input className="p-2 mb-3 bg-gray-800 rounded-lg dark:bg-gray-200" type="password" />
                 </div>
+                <div className="flex justify-between">
                 <Link className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black" type="submit" href="/calender" >Login</Link>
+                <Link href="/register" className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black">Register</Link>
+
             </div>
         </div>
-
+    </div>
     );
 }
