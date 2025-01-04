4fe611f6a596bb7001cd9610c27f8ccd2801f044
im23b-terenzianie <terenzianie@bzz.ch>
2025-01-04 16:12:24 +0100

    Changed the main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import '../app/globals.css';
 export default function Home() {
     return (
         <Base>
-            <h1>Home</h1>
+            <h1 className="text-4xl text-center">It's a great day to be organised!</h1>
+            <div className={"flex justify-center"}>
+                This is my first fullstack project. I am using Next.js, Tailwind CSS, and MongoDB. I am also using Vercel for deployment. I am excited to see how this project will turn out.
+            </div>
+
         </Base>
     );
 }
