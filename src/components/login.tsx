68309f9f9288925907addb485c429a671213a966
im23b-terenzianie <terenzianie@bzz.ch>
2025-03-01 12:03:58 +0100

    Backend am erstellen

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,24 +1,26 @@
 import '../../app/globals.css';
-import Link from "next/link";
+import { login, signup } from './action';
 
 export default function Login() {
     return (
         <div className="flex justify-center items-center h-screen bg-gray-200 dark:bg-gray-700">
             <div className="bg-black p-5 rounded-lg dark:bg-white">
                 <h1 className="text-2xl text-white text-center dark:text-black">Login</h1>
-                <div className="flex flex-col">
-                    <label className="text-white dark:text-black">Username</label>
-                    <input className="p-2 mb-3 bg-gray-800 rounded-lg dark:bg-gray-200" type="text" />
-                </div>
-                <div className="flex flex-col">
-                    <label className="text-white dark:text-black">Password</label>
-                    <input className="p-2 mb-3 bg-gray-800 rounded-lg dark:bg-gray-200" type="password" />
-                </div>
-                <div className="flex justify-between">
-                <Link className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black" type="submit" href="/calender" >Login</Link>
-                <Link href="/register" className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black">Register</Link>
+                <form>
+                    <div className="flex flex-col">
+                        <label htmlFor="email" className="text-white dark:text-black">Email:</label>
+                        <input id="email" name="email" type="email" required className="p-2 mb-3 bg-gray-800 rounded-lg dark:bg-gray-200" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="password" className="text-white dark:text-black">Password:</label>
+                        <input id="password" name="password" type="password" required className="p-2 mb-3 bg-gray-800 rounded-lg dark:bg-gray-200" />
+                    </div>
+                    <div className="flex justify-between">
+                        <button formAction={login} className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black">Log in</button>
+                        <button formAction={signup} className="p-2 bg-gray-800 rounded-lg dark:bg-gray-200 text-black">Sign up</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
     );
 }
